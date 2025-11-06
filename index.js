f102539882e156941c7749f1cdf60dd1a2c72dda
Sebastian Noe <sebastian.schneider@boxine.de>
2025-11-06 22:01:58 +0100

    explicitely define theme to use

--- index.js
+++ index.js
@@ -50,13 +50,20 @@ const batteryStore = {};
   };
 
   console.log("Adding authentication entry to browser's local storage...");
+  const defaultTheme = { theme: "eink" };
+  const themeToUse = process.env.HA_THEME
+    ? { theme: process.env.HA_THEME }
+    : defaultTheme;
+
   await page.evaluate(
-    (hassTokens, selectedLanguage) => {
+    (hassTokens, selectedLanguage, selectedTheme) => {
       localStorage.setItem("hassTokens", hassTokens);
       localStorage.setItem("selectedLanguage", selectedLanguage);
+      localStorage.setItem("selectedTheme", selectedTheme);
     },
     JSON.stringify(hassTokens),
-    JSON.stringify(config.language)
+    JSON.stringify(config.language),
+    JSON.stringify(themeToUse)
   );
 
   page.close();
@@ -317,7 +324,7 @@ function convertImageToKindleCompatiblePngAsync(
     if (pageConfig.imageFormat !== 'bmp') {
       gmInstance = gmInstance.quality(100);
     }
-    
+
     gmInstance.write(outputPath, (err) => {
       if (err) {
         reject(err);
